276c4db0e7e7c11a50fc647d67208f8538ee27c9
Dirceu Heineck <dirceu.heineck@fw7.com.br>
2022-07-21 15:36:00 -0300

    feat(api): update data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 //Rotas
 const index = require('./routes/index')
@@ -7,7 +8,7 @@ const personRoute = require('./routes/personRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(cors())
 app.use('/', index)
 app.use('/person', personRoute)
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,37 +1,48 @@
 const express = require('express')
 const router = express.Router()
-const axios = express.axios
 
 const data = [
   {
     id: 1,
     name: 'Dirceu ',
-    company: 'FW7 Soluções'
+    company: 'FW7 Soluções',
+    city: 'Blumenau',
+    age: '40'
   },
   {
     id: 2,
     name: 'Pablo',
-    company: 'FW7 Soluções'
+    company: 'FW7 Soluções',
+    city: 'Blumenau',
+    age: '-'
   },
   {
     id: 3,
     name: 'Rafael',
-    company: 'FW7 Soluções'
+    company: 'FW7 Soluções',
+    city: 'Blumenau',
+    age: '-'
   },
   {
     id: 4,
     name: 'Henrique',
-    company: 'FW7 Soluções'
+    company: 'FW7 Soluções',
+    city: 'Blumenau',
+    age: '-'
   },
   {
     id: 5,
     name: 'Roberto',
-    company: 'FW7 Soluções'
+    company: 'FW7 Soluções',
+    city: 'Blumenau',
+    age: '-'
   },
   {
     id: 6,
     name: 'Anderson',
-    company: 'FW7 Soluções'
+    company: 'FW7 Soluções',
+    city: 'Blumenau',
+    age: '-'
   }
 ]
 
