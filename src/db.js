b2d42ef2f8c39b1c18b5650998768d952fee0411
Dirceu Heineck <dirceu.heineck@fw7.com.br>
2022-07-22 14:35:07 -0300

    feat(api): updated delete db

--- src/db.js
+++ src/db.js
@@ -14,7 +14,24 @@ connect()
 
 async function selectProducts() {
   const conn = await connect()
-  const [rows] = await conn.query('SELECT * FROM tb_produto LIMIT 5')
+  const [rows] = await conn.query(
+    'SELECT * FROM tb_produto WHERE pr_codpro < 600'
+  )
   return rows
 }
-module.exports = { selectProducts }
+
+async function deleteProducts(pr_codpro) {
+  const connd = await connect()
+  const [rows1] = await connd.query(
+    `delete  from tb_ped_item WHERE pi_codpro =  ${pr_codpro}`
+  )
+  const [rows3] = await connd.query(
+    `DELETE FROM tb_pro_cors_gupo_item WHERE cg_codpro = ${pr_codpro}`
+  )
+  const [rows2] = await connd.query(
+    `DELETE FROM tb_produto WHERE pr_codpro = ${pr_codpro}`
+  )
+
+  return 'Deletado com sucesso.' + rows1
+}
+module.exports = { selectProducts, deleteProducts }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,55 +3,15 @@ const router = express.Router()
 
 ;(async () => {
   const db = require('../db')
-  const products = await db.selectProducts()
 
-  // const data = [
-  //   {
-  //     id: 1,
-  //     name: 'Dirceu ',
-  //     company: 'FW7 Soluções',
-  //     city: 'Blumenau',
-  //     age: '40'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Pablo',
-  //     company: 'FW7 Soluções',
-  //     city: 'Blumenau',
-  //     age: '-'
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Rafael',
-  //     company: 'FW7 Soluções',
-  //     city: 'Blumenau',
-  //     age: '-'
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Henrique',
-  //     company: 'FW7 Soluções',
-  //     city: 'Blumenau',
-  //     age: '-'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Roberto',
-  //     company: 'FW7 Soluções',
-  //     city: 'Blumenau',
-  //     age: '-'
-  //   },
-  //   {
-  //     id: 6,
-  //     name: 'Anderson',
-  //     company: 'FW7 Soluções',
-  //     city: 'Blumenau',
-  //     age: '-'
-  //   }
-  // ]
-
-  router.get('/employees', function (req, res, next) {
+  router.get('/employees', async function (req, res, next) {
+    const products = await db.selectProducts()
     res.status(200).send(products)
+  })
+  router.delete('/product', function (req, res, next) {
+    const id = req.query.id
+    const returnDb = db.deleteProducts(id)
+    res.status(200).send(returnDb + '-' + id)
   })
 })()
 module.exports = router
