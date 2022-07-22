21d5748a27982fea90664463c33ba28b70759250
Dirceu Heineck <dirceu.heineck@fw7.com.br>
2022-07-22 09:57:26 -0300

    feat(api): update identation code

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-const express = require('express')
 const app = express()
 const cors = require('cors')
+const express = require('express')
 
 //Rotas
 const index = require('./routes/index')
 const personRoute = require('./routes/personRoute')
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use('/', index)
+app.use(express.json())
 app.use('/person', personRoute)
+app.use(express.urlencoded({ extended: true }))
 
 module.exports = app
--- src/routes/personRoute.js
+++ src/routes/personRoute.js
@@ -1,11 +1,11 @@
-const express = require('express')
 const router = express.Router()
+const express = require('express')
 const controller = require('../controllers/personController')
 
 router.get('/', controller.get)
 router.get('/:id', controller.getById)
-router.post('/', controller.post)
 router.put('/:id', controller.put)
+router.post('/', controller.post)
 router.delete('/:id', controller.delete)
 
 module.exports = router
